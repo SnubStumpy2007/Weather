fd1ef7f93efa312f6d32d6721d0cd703445ec6db
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-04 22:04:33 -0400

    working on functions

--- weather.js
+++ weather.js
@@ -1,13 +1,34 @@
-import axios from "axios"
+import axios from "axios";
 
 // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York
 
-
-export function getWeather (lat, lon, timezone) {
-    return axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime", {
-         params: {
+export function getWeather(lat, lon, timezone) {
+  return axios.get(
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+    {
+      params: {
         latitude: lat,
         longitude: lon,
         timezone,
-    }})
+      },
+    }
+  ).then(({data}) =>{
+    return{
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data),
+    }
+ 
+  }  );
 }
+
+
+function parseCurrentWeather({ current_weather, daily }) {
+    return {
+
+    }
+}
+
+function parseDailyWeather
+
+function parseHourlyWeather
